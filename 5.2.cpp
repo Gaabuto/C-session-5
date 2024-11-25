3e00002292d962e7e6c79b98b9c3aedb28fcc4db
Gaabuto <khanh040604@gmail.com>
2024-11-25 20:41:56 +0700

    5.2

---
+++ 5.2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int i;
+	printf("moi ban nhap 1 so ngau nhien: ");
+	scanf ("%d", &i);
+	while (i != 123) {
+		printf ("sai roi dien lai di!!!\n");
+		printf("moi ban nhap 1 so ngau nhien: ");
+		scanf ("%d", &i);
+			
+		}
+	
+	printf("chuc mung ban da doan chinh xac");
+	return 0;
+}
