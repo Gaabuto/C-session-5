e03290726491c3ad4f03b0cc98b244b286c363bb
Gaabuto <khanh040604@gmail.com>
2024-11-26 08:32:57 +0700

    5.6.cpp

--- 5.6.cpp
+++ 5.6.cpp
@@ -6,8 +6,10 @@ int main(){
 	scanf("%d",&a);
 	printf("moi ban nhap so thu 2: ");
 	scanf("%d",&b);
-	printf("CALCULATOR\n 1. tong 2 so\n 2. hieu 2 so\n 3. tich 2 so\n 4. thuong 2 so\n 5. thoat\n Lua chon cua ban la: ");
-	scanf("%d",&c);
+	do{
+		
+	printf("\nCALCULATOR\n 1. tong 2 so\n 2. hieu 2 so\n 3. tich 2 so\n 4. thuong 2 so\n 5. thoat\n Lua chon cua ban la: ");
+	scanf("%d" ,&c);
 	switch(c){
 		case 1:
 			printf("%d + %d = %d", a, b, a+b);
@@ -21,16 +23,19 @@ int main(){
 		case 4:
 			if(b ==0){
 				printf("so thu 2 khong the bang k, nhap lai di");
+			}else{
+				printf("%d : %d = %f", a, b,(float)a/b);
 			}
-			printf("%d : %d = %f", a, b,a/b);
 			break;
 		case 5:
 			printf("ggwp");
 			break;
 		default:
 			printf("chon sai roi!!! nhap lai di");
-			break;
+			break;	
+			
 	}
 	
-	return 0;
+	}while(c!=5);
+		return 0;
 }
